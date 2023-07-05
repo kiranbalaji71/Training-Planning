edc5d9aeaed4dd4742bd9bd37e3b8fd872ca2fc6
kiranbalaji71 <kiran19701@gmail.com>
2023-07-05 16:34:42 +0530

    day 3 task completed

---
+++ Day -3 Python Pandas Framework(1)/merge_csv.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+file1 = input("Enter your csv file 1 path: ")
+file2 = input("Enter your csv file 2 path: ")
+
+output_file = input("Enter your output csv file path: ")
+
+dataset1 = pd.read_csv(file1)
+dataset2 = pd.read_csv(file2)
+
+output = pd.merge(dataset1,dataset2, how='left').fillna('NaN')
+
+output.to_csv(output_file)
+print('csv file created!!!')
+
