db843e3e2061016f66811189629ab810ef89e613
kiranbalaji71 <kiran19701@gmail.com>
2023-07-13 18:17:16 +0530

    Day 9 task completed

--- Day -9 Python Encoding and Decoding/encodeToDecode3.py
+++ Day -9 Python Encoding and Decoding/encodeToDecode3.py
@@ -3,15 +3,21 @@ with open('./textFile/spanish.txt','r',encoding='utf-8') as spfile:
         spresult.write(spfile.read())
         if(spfile.read() == spresult.read()):
             print('spainish file is coming out correctly')
+        else:
+            print('spanish file is not coming out correctly')
 
 with open('./textFile/japanese.txt','r',encoding='utf-8') as jafile:
     with open('./textFile/jaoutput.txt','w+',encoding='utf-16') as jaresult:
         jaresult.write(jafile.read())
         if(jafile.read() == jaresult.read()):
             print('japanese file is coming out correctly')
+        else:
+            print('japanese file is not coming out correctly')
 
 with open('./textFile/russian.txt','r',encoding='utf-8') as rufile:
     with open('./textFile/ruoutput.txt','w+',encoding='utf-16') as ruresult:
         ruresult.write(rufile.read())
         if(rufile.read() == ruresult.read()):
             print('russian file is coming out correctly')
+        else:
+            print('russian file is not coming out correctly')
